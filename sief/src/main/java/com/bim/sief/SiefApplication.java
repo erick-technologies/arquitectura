d4c49283ddad864ff870f9e31c54a1697d75cab5
erick-technologies <erickcasme@gmail.com>
2025-03-19 16:40:21 -0600

    first commit

---
+++ sief/src/main/java/com/bim/sief/SiefApplication.java
@@ -0,0 +1,13 @@
+package com.bim.sief;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiefApplication.class, args);
+	}
+
+}
